e1e12019ed361e4f628a599eff2636141a828bf8
Butthead32 <Butthead2048@gmail.com>
2025-05-28 06:20:27 +0300

    new test

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const outputElement = document.getElementById('output');
+    outputElement.textContent = "ВИЖУ ЕГО";
+    console.log("Кнопка была нажата - сообщение выведено");
+}

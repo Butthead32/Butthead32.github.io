633b588f219e48452252a84166377757ad513b45
Butthead32 <Butthead2048@gmail.com>
2025-05-28 07:58:32 +0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let pageId = 0;
+
+async function loadJson() {
+    try {
+        const response = await fetch('data.json');
+        if (!response.ok) throw new Error('Ошибка загрузки');
+        const data = await response.json();
+        console.log('Данные:', data);
+        return data;  // Можно вернуть данные для дальнейшего использования
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+}
+
+// Вызов функции
+loadJson().then(data => {
+    // Действия с данными
+});
+
 const pagesContent = ["text 0", "text 1", "text 2", "text 3", "text 4", "text 5"];
 
 function changePage() {

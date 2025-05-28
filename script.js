c7f162106f4f2e43ee235ae9c966dcdafb0679e6
Butthead32 <Butthead2048@gmail.com>
2025-05-28 07:10:18 +0300

    ff

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function showMessage() {
-    const outputElement = document.getElementById('output');
-    outputElement.textContent = "ВИЖУ ЕГО";
-    console.log("Кнопка была нажата - сообщение выведено");
+let pageId = 0;
+const pagesContent = ["text 0", "text 1", "text 2", "text 3", "text 4", "text 5"];
+
+function changePage() {
+    const outputElement = document.getElementById('pageContent');
+    outputElement.textContent = pagesContent[pageId];
+}
+
+function cyclePage() {
+    pageId++;
+    changePage();
+}
+
+function jumpToPage() {
+    pageId = 1;
+    changePage();
 }
